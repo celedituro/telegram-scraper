5420ea9bb8278ca78b4e4bf2b7dff9c808007f87
celedituro <cdituro@fi.uba.ar>
2023-07-29 19:52:34 -0300

    feat: integrate telegram bot

--- myscraper/database.py
+++ myscraper/database.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# Access the environment variables as you normally would using os.environ
+# Access the environment variables
 db_name = os.environ.get('DB_NAME')
 db_user = os.environ.get('DB_USER')
 db_password = os.environ.get('DB_PASSWORD')
--- myscraper/main.py
+++ myscraper/main.py
@@ -1,12 +1,31 @@
-from fastapi import FastAPI, HTTPException
 from .database import Database
-            
-app = FastAPI()
+from .telegram_bot import TelegramBot
+import threading
+from .api import app
+import uvicorn
+
+# Init database 
 db = Database()
 db.create_message_table()
 db.close()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def run_api():
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+bot = TelegramBot()
+# Spawn bot thread
+bot_thread = threading.Thread(target=bot.start_bot)
+print("Spawn bot thread")
+bot_thread.start()
+
+# Spawn API thread
+api_thread = threading.Thread(target=run_api)
+print("Spawn api thread")
+api_thread.start()
+
+# Wait bot and API threads to join
+bot_thread.join()
+print("Bot thread finished")
+api_thread.join()
+print("Api thread finished")
 
