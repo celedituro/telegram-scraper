349474b61e3318c63e8db7900c24f8f2f9fa31ff
celedituro <cdituro@fi.uba.ar>
2023-08-02 11:38:01 -0300

    feat: get session code

--- myscraper/main.py
+++ myscraper/main.py
@@ -1,37 +1,5 @@
-from .database import Database
-from .bot import TelegramBot
-import threading
 from .api import app
 import uvicorn
 
-def run_db():
-    try:
-        db = Database()
-        db.create_message_table()
-        # Perform other operations with the database if needed
-    except Exception as e:
-        print("Error during application execution:", e)
-    finally:
-        db.close()
-
-def run_api():
+if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-bot = TelegramBot()
-
-# Spawn bot thread
-bot_thread = threading.Thread(target=bot.start_bot)
-print("Spawn bot thread")
-bot_thread.start()
-
-# Spawn API thread
-api_thread = threading.Thread(target=run_api)
-print("Spawn api thread")
-api_thread.start()
-
-# Wait bot and API threads to join
-bot_thread.join()
-print("Bot thread finished")
-api_thread.join()
-print("Api thread finished")
-
