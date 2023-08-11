5d1e6a9e832bd39bea4f34db467c3fd1374025bb
celedituro <cdituro@fi.uba.ar>
2023-08-10 21:01:40 -0300

    refactor: rename rule file

--- yara/detect_rule.py
+++ yara/detect_rule.py
@@ -5,7 +5,7 @@ script_directory = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(script_directory, "..", "app", "messages.txt")
 
 # Load rules from a file
-rules = yara.compile(filepath="detect_mercadolibre.yara")
+rules = yara.compile(filepath="mercadolibre.yara")
 
 # Scan messages.txt in search of coincidences
 matches = rules.match(file_path)
